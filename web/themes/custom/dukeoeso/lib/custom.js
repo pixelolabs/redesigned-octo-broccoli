b8ccb2fc623d562c40da3e08b69ea1f9c7108429
Gaurav Jaiswal <dev@pixelolabs.com>
2022-08-23 20:40:04 +0530

    Header, footer and generic componenets

--- web/themes/custom/dukeoeso/lib/custom.js
+++ web/themes/custom/dukeoeso/lib/custom.js
@@ -16,4 +16,41 @@ jQuery(function () {
         prevEl: ".swiper-button-prev-hero-banner",
       },
     });
+
+    $(window).scroll(function(){
+      var sticky = $('.js__main-header'),
+          scroll = $(window).scrollTop();
+    
+      if (scroll >= 100) sticky.addClass('fixed');
+      else sticky.removeClass('fixed');
+    });
+
+/*Accrodian*/
+    $(".js__accordian")
+    .children("li")
+    .children("h5,h6,h3")
+    .click(function (e) {
+      if ($(this).parent("li").children(".content").css("display") == "none") {
+        $(this)
+          .parent("li")
+          .parent(".js__accordian")
+          .children("li")
+          .children(".content")
+          .hide();
+        $(this)
+          .parent("li")
+          .parent(".js__accordian")
+          .children("li")
+          .removeClass("active");
+        $(this).parent("li").children(".content").slideDown();
+        $(this).parent("li").addClass("active");
+        /* $('html,body').animate({
+          scrollTop: $(this).offset().top - 120
+        }, 500);*/
+      } else {
+        $(this).parent("li").children(".content").slideUp();
+        $(this).parent("li").removeClass("active");
+      }
+    });
   })
+
